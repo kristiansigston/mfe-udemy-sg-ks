3379a20e38491866be373b56004b0b569221375d
Kristian Sigston <kristian.sigston@sabiogroup.com>
2021-11-25 20:34:23 +0200

    namespace css classes

--- project/container/src/app/index.js
+++ project/container/src/app/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "../components/header";
 import MarketingMount from "../components/marketingApp";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingMount />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingMount />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
